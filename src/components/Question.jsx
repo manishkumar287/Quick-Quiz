78f67bed6a7d60a7ebe88f24d17560482e14d439
manishkumar287 <manishkumarchaudhary287@gmail.com>
2021-08-26 19:36:43 +0530

    quizes

---
+++ src/components/Question.jsx
@@ -0,0 +1,49 @@
+const Questions=[{
+    question: "FISH : SCHOOL",
+    Qlist: [
+        {answer:"wolf : pack",isCorrect:true},
+        {answer:"cow : farm",isCorrect:false},
+        {answer:"herd: peacock",isCorrect:false},
+        {answer:"elephant: jungle",isCorrect:false}
+
+    ]
+},
+{
+    question: "Look at this series: 2, 1, (1/2), (1/4), ... What number should come next?",
+    Qlist: [
+        {answer:"1/3",isCorrect:false},
+        {answer:"1/8",isCorrect:true},
+        {answer:"2/8",isCorrect:false},
+        {answer:"1/16",isCorrect:false}
+    ]
+},
+{
+    question: "Look at this series: 7, 10, 8, 11, 9, 12, ... What number should come next?",
+    Qlist: [
+        {answer:"7",isCorrect:false},
+        {answer:"10",isCorrect:true},
+        {answer:"12",isCorrect:false},
+        {answer:"13",isCorrect:false}
+    ]
+},
+{
+    question: "Look at this series: 36, 34, 30, 28, 24, ... What number should come next?",
+    Qlist: [
+        {answer:"20",isCorrect:false},
+        {answer:"22",isCorrect:true},
+        {answer:"23",isCorrect:false},
+        {answer:"26",isCorrect:false}
+    ]
+},
+{
+    question: "Look at this series: 22, 21, 23, 22, 24, 23, ... What number should come next?",
+    Qlist: [
+        {answer:"22",isCorrect:false},
+        {answer:"24",isCorrect:false},
+        {answer:"25",isCorrect:true},
+        {answer:"26",isCorrect:false}
+    ]
+}
+]
+
+export default Questions;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,70 @@
+import React,{useState} from 'react';
+import Questions from "./Question";
+
+
+function Quiz(){
+
+    const [currentQuestion, setQuestion]=useState(0);
+    const [count, setCount]=useState(0);
+    const [clicked, setClicked]=useState(false);
+    const [show,setShow]=useState(false);
+    
+    function nextClick(){
+        setClicked(false);
+        if(currentQuestion<Questions.length-1){
+            setQuestion(currentQuestion+1);
+        }
+        else{
+            setShow(true);
+        }
+        
+    }
+    function handleClick(isCorrect){
+        if(isCorrect){
+            setCount(count+1);
+        }
+        else{
+            setClicked(true);
+        }
+    }
+
+    return(<>
+        {show
+        ?
+        <div className="final">
+            <h1>Hurray Your Quiz Is Completed!!</h1>
+            <h2>Your Score is {count} out of {Questions.length}</h2>
+            <h3>Thanks!</h3>
+        </div>
+        
+        :
+    <div className="var">
+        <header>
+            <h1>Quick Quizzz </h1>
+            <h2>Question {currentQuestion+1} of {Questions.length}</h2>
+        </header>
+        
+<div className="Options">
+        <div className="Question">
+           <h1> {Questions[currentQuestion].question} </h1>
+        </div>
+        <div className="note">
+            {Questions[currentQuestion].Qlist.map((a)=>(
+           <li value={currentQuestion+1}>
+                <button className="note-button" onClick={()=>handleClick(a.isCorrect)}>{a.answer}</button>
+            </li>
+            
+        ))}
+        </div>
+        <div>
+            <button className="note-button-next" onClick={nextClick}>Next</button>
+        </div>
+</div>
+
+    </div>
+        }
+        
+        </>
+    )
+}
+export default Quiz;
